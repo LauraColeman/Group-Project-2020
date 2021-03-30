87afebf5cd46666b1d4b1f8bee61ce2af310bed2
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2021-03-30 16:35:27 +0100

    Finshed Development of the Google Chrome Plugin

--- SketchDetect/background.js
+++ SketchDetect/background.js
@@ -1,1 +1,27 @@
-console.log("The URL of this page is: " + window.location.href);
+var webpage = window.location.href;
+
+var trustworthy = true;
+var warnings = ['@','registered','update','security','billing','account','secur','veri','com-','update','support','service','auth','confirm','xn--']
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+  
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            for(var i = 0; i < warnings.length; i++)
+            {
+                if(tabs[0].url.includes(warnings[i]) && !tabs[0].url.includes("google") || ((tabs[0].url.length) > 54) && !tabs[0].url.includes("google"))
+                {
+                    if(window.confirm("Uh Oh! This site may be harmful to your computer. Would you like to leave?"))
+                    {
+                        chrome.tabs.update({url: "https://www.google.com"});
+                        return;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }                   
+        });
+    }
+  })
