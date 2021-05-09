4b75d72fd6fae638015ef4da488d48fd10ed9e15
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2021-05-09 22:03:27 +0100

    Finished Extension
    > Added Icons for Display on Chrome
    > Created HTML and CSS Interface for extension popup
    > Added comments throughout code

--- SketchDetect/background.js
+++ SketchDetect/background.js
@@ -1,26 +1,26 @@
-var webpage = window.location.href;
+var warnings = ['@','registered','update','security','billing','account','secur','veri','com-','update','support','service','auth','confirm','xn--'] //Common URL occurences that need to be checked
 
-var trustworthy = true;
-var warnings = ['@','registered','update','security','billing','account','secur','veri','com-','update','support','service','auth','confirm','xn--']
-
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) { //Runs when current tab in Chrome is updated
+    if (changeInfo.status == 'complete') { //When the update is complete
   
-        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-            for(var i = 0; i < warnings.length; i++)
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => { //checks active tab
+            for(var i = 0; i < warnings.length; i++) //runs through every string in 'warnings' array
             {
-                if(tabs[0].url.includes(warnings[i]) && !tabs[0].url.includes("google") || ((tabs[0].url.length) > 54) && !tabs[0].url.includes("google"))
+                if(
+                tabs[0].url.includes(warnings[i]) && !tabs[0].url.includes("google") ||  //If word is found in url (Ignores words if part of Google search)
+                ((tabs[0].url.length) > 54) && !tabs[0].url.includes("google")) //if URL is longer than 54 characters (Ignores if part of Google search)
                 {
-                    if(window.confirm("Uh Oh! This site may be harmful to your computer. Would you like to leave?"))
-                    {
-                        chrome.tabs.update({url: "https://www.google.com"});
+                    if(window.confirm("Uh Oh! This site may be harmful to your computer. Would you like to leave?")) //Display popup window informing user
+                    { //If user selects 'Okay'
+                        chrome.tabs.update({url: "https://lauracoleman.github.io/"}); //re-direct away from site to educational website
                         return;
                     }
                     else
                     {
-                        break;
+                        break; //if user selects 'cancel', do nothing
                     }
                 }
+
             }                   
         });
     }
